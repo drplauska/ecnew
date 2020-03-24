9b1ed31dbf8fcc99a3caa01cadd2448d53ac76ef
Ernestas <drplauska@gmail.com>
2020-03-24 20:40:44 +0200

    test lint

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { Landing } from "./pages";
+import { Landing } from './pages';
 
 function Router() {
+  const siaip = 123;
   return (
     <BrowserRouter>
       <Switch>
